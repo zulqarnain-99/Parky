d4bcf4909cadecfac960fe6cf420fe1d92682197
Zulqarnain Qasim <zulqarnain.qasim@outlook.com>
2022-08-10 23:47:27 +0500

    Trail model created

--- ParkyAPI/Models/NationalPark.cs
+++ ParkyAPI/Models/NationalPark.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ParkyAPI.Models
 {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-    public class NationalParkDto
+    public class NationalPark
 
     {
         [Key]
---
+++ ParkyAPI/Models/Trail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkyAPI.Models
+{
+    public class Trail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Distance { get; set; }
+        public enum DifficultType { Easy, Moderate, Difficult, Expert}
+        public DifficultType Difficulty { get; set; }
+
+        [Required]
+        public int NationalParkId { get; set; }
+
+        [ForeignKey("NationalParkId")]
+        public NationalPark NationalPark { get; set; }
+    }
+}
